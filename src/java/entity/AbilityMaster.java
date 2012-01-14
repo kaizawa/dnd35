535a5f5d24d841d7ba22fe27c5f55e02d65923e7
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2012-01-14 22:59:27 +0900

    add import

--- src/java/entity/AbilityMaster.java
+++ src/java/entity/AbilityMaster.java
@@ -7,16 +7,7 @@ package entity;
 
 import java.io.Serializable;
 import java.util.Collection;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  *
